c00643540978abe9612b30dfeb2f0e88eb9c2896
chenbin.li <lichenbin1119@gmail.com>
2023-06-19 14:02:57 +0800

    style: changed some characters and variable names

--- handwritings/throttle.ts
+++ handwritings/throttle.ts
@@ -1,15 +1,15 @@
 // 节流函数： 将多次执行改为每隔一段时间执行
 
 // 时间戳版本
-// 第一次触发事件时立即执行，以后每过 wait 秒之后才执行一次，并且最后一次触发事件不会被执行
-function throttle1(fn: Function, wait: number = 500): () => void {
+// 第一次触发事件时立即执行，以后每过 waitTime 秒之后才执行一次，并且最后一次触发事件不会被执行
+function throttle_timestamp(fn: Function, waitTime: number = 500): () => void {
   // 上一次执行的时间
   let lasttime = 0
   return function (...args) {
     // 当前时间
     let now = +new Date()
     // 比较当前时间和上一次执行时间
-    if (now - lasttime > wait) {
+    if (now - lasttime > waitTime) {
       lasttime = now
       fn.apply(this, args)
     }
@@ -19,7 +19,7 @@ function throttle1(fn: Function, wait: number = 500): () => void {
 //定时器版本
 // 在第一次触发时不会执行，而是在 delay 秒之后才执行，当最后一次停止触发后，还会再执行一次函数
 
-function throttle2(fn: Function, wait: number): () => void {
+function throttle_timer(fn: Function, waitTime: number): () => void {
   // 保存定时器
   let timer: null | number = null
   return function (...args) {
@@ -27,7 +27,7 @@ function throttle2(fn: Function, wait: number): () => void {
       timer = setTimeout(() => {
         fn.apply(this, args)
         timer = null
-      }, wait)
+      }, waitTime)
     }
   }
 }
